c21aa22846c6a116bf0e98f9f5b3dbea7e288905
minguk-cucu <minkuk0118@gmail.com>
2025-10-01 09:24:40 +0900

    [Refactor] selenium version 수정, init 파일 배열 가독성 수정

--- weekly_crawl/__init__.py
+++ weekly_crawl/__init__.py
@@ -1,8 +1,8 @@
 import os
 
 CSV_FOLDER_NAME = "csv"
-YEARS = [2020, 2021, 2022, 2023, 2024, 2025
-         ]
+YEARS = [2020, 2021, 2022, 2023, 2024, 2025]
+
 CSV_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), CSV_FOLDER_NAME)
 if not os.path.exists(CSV_DIR):
     os.mkdir(CSV_DIR)
